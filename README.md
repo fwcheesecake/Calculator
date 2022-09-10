6eaeb95cae60defb78024e4f8811c9f6f8916a60
Antonio Reyna <antoniore102001@gmail.com>
2022-09-10 00:00:52 -0500

    Minor changes

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Calculator
-Java Calculator
+# Simple Expression Evaluator
+This is an expression evaluator that I've made for my Logical and Functional Programming class
+
+## Table of Contents
+- [Simple Expression Evaluator](#simple-expression-evaluator)
+	- [Table of Contents](#table-of-contents)
+	- [Download](#download)
+	- [Running the project](#running-the-project)
+	- [Usage](#usage)
+
+
+
+## Download
+Download the project using the vcs tools in IntelliJ Idea or using git from your terminal
+```
+git clone https://github.com/fwcheesecake/Calculator.git
+```
+
+## Running the project
+To run the project you need to have JDK 15 installed. If you're using an IDE you can simply press the run button and that's it.
+
+First you need cd into the project to compile the necessary files:
+```
+cd Calculator
+javac *.java
+javac components/*.java
+```
+
+Then run the project with
+```
+java Main
+```
+
+## Usage
+It's simple, you only need to type an expression to be evaluated and then press the equals symbol to perform the operations.
+![Calculator evaluating an expression](img/../src/img/Screenshot%201.png)
+![Calculator evaluating an expression](img/../src/img/Screenshot%202.png)
--- src/Calculus.java
+++ src/Calculus.java
@@ -1,6 +1,20 @@
 import java.util.Stack;
 
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  Antonio Reyna Espinoza
+ * @version 1.0
+ * @since   20022-09-06
+ */
 class Calculus {
+    /**
+     * This method takes an operator as a character and returns
+     * its precedence in an integer value
+     * @param ch This is operator from which the precedence is calculated
+     * @return This returns the precedence from an operator
+     */
     private static int Prec(char ch) {
         return switch (ch) {
             case '+', '-' -> 1;
@@ -10,47 +24,64 @@ class Calculus {
         };
     }
 
+    /**
+     * This method takes an expression as a string in infix form
+     * and transforms it to postfix form in order to perform the
+     * operations easily
+     * @param exp This is the expression to be converted
+     * @return This returns the expression converted to postfix
+     */
     public static String infixToPostfix(String exp) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < exp.length(); ++i) {
             for(; i < exp.length() &&
                         (Character.isDigit(exp.charAt(i)) || exp.charAt(i) == '.'); i++)
-                result += exp.charAt(i);
+                result.append(exp.charAt(i));
             if(result.length() > 0)
                 if(Character.isDigit(result.charAt(result.length() - 1)))
-                    result += " ";
+                    result.append(" ");
             if(i == exp.length())
                 break;
 
             char c = exp.charAt(i);
-            if(c == ' ') {
-            }
-            else if (c == '(')
-                stack.push(c);
-            else if (c == ')') {
-                while (!stack.isEmpty() && stack.peek() != '(')
-                    result += stack.pop() + " ";
-                stack.pop();
-            } else {
-                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()))
-                    result += stack.pop() + " ";
-                stack.push(c);
+            switch (c) {
+                case ' ':
+                    break;
+                case '(':
+                    stack.push(c);
+                    break;
+                case ')':
+                    while (!stack.isEmpty() && stack.peek() != '(')
+                        result.append(stack.pop()).append(" ");
+                    stack.pop();
+                    break;
+                default:
+                    while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek()))
+                        result.append(stack.pop()).append(" ");
+                    stack.push(c);
+                    break;
             }
         }
 
         while (!stack.isEmpty()) {
             if(stack.peek() == '(')
                 return "Syntax error";
-            result += " " + stack.pop();
+            result.append(" ").append(stack.pop());
         }
-        return result;
+        return result.toString();
     }
 
+    /**
+     * This method is used to iterate over the postfix
+     * expression and calculate the final result
+     * @param postfix This is the expression in postfix form
+     * @return This returns the last value in the stack
+     */
     public static String doOperations(String postfix) {
-        String[] exp = postfix.split("\\s");
+        String[] exp = postfix.split("\\s*");
         Stack<String> stack = new Stack<>();
 
         for(String term : exp) {
@@ -67,6 +98,13 @@ class Calculus {
         return stack.pop();
     }
 
+    /**
+     * This method is used to perform each operation individually
+     * @param aS This is the first operand of the expression
+     * @param bS This is the second operand of the experssion
+     * @param operator This is the operator of the expression
+     * @return This returns the result of a simple expression
+     */
     private static double operation(String aS, String bS, String operator) {
         double a = Double.parseDouble(aS);
         double b = Double.parseDouble(bS);
